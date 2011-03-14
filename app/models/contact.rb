6f7fb604eb8531f945edcbe9fd4e15777ec7d2f8
Peter Zlatnar <peter.zlatnar@gmail.com>
2011-03-14 13:54:48 +0100

    Replace topic method with association

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,8 +4,6 @@ class Contact < ActiveRecord::Base
   validates :email, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
   validates :order_number, :format => {:with => /(^$)|(^R\d{9}$)/i, :message => I18n.t("invalid_order_number")}
   
-  def topic
-    @topic ||= Topic.find(topic_id)
-  end
-  
+  belongs_to :topic
+ 
 end
