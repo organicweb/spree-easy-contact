3aae06c90515c39e71039b336a9bbc98ca80929a
Peter Ritchie <peterr@lockside.co.uk>
2012-05-15 12:05:30 +0100

    Modifying admin resources to work with spree v1

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,2 +1,21 @@
 class Admin::ContactsController < Spree::Admin::ResourceController
+  def model_class
+    ::Contact
+  end
+
+  def new_object_url(options = {})
+    main_app.new_polymorphic_url([:admin, model_class], options)
+  end
+
+  def edit_object_url(object, options = {})
+    main_app.edit_admin_contact_url object, options
+  end
+
+  def object_url(object = nil, options = {})
+    main_app.admin_contact_url object, options
+  end
+
+  def collection_url(options = {})
+    main_app.polymorphic_url([:admin, model_class], options)
+  end
 end
--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -1,11 +1,21 @@
 class Admin::TopicsController < Spree::Admin::ResourceController
-  resource_controller
-  
-  create.wants.html {redirect_to collection_path}
-  update.wants.html {redirect_to collection_path}
-  
-  new_action.response do |wants|
-    wants.html {render :action => :new, :layout => !request.xhr?}
+   def model_class
+    ::Topic
   end
-  
+
+  def new_object_url(options = {})
+    main_app.new_polymorphic_url([:admin, model_class], options)
+  end
+
+  def edit_object_url(object, options = {})
+    main_app.edit_admin_topic_url object, options
+  end
+
+  def object_url(object = nil, options = {})
+    main_app.admin_topic_url object, options
+  end
+
+  def collection_url(options = {})
+    main_app.polymorphic_url([:admin, model_class], options)
+  end
 end
