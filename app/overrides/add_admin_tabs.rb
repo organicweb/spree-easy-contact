87fd42221e0db3bc15ecf8e44cd10dd812954125
Peter Ritchie <peterr@lockside.co.uk>
2012-05-15 11:29:00 +0100

    Using correct path for admin tab

--- app/overrides/add_admin_tabs.rb
+++ app/overrides/add_admin_tabs.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
   :name => "converted_admin_tabs_536522210",
   :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-  :text => "<%= tab(:contacts) %>",
+  :text => "<%= tab(:contacts, url: main_app.admin_contacts_path) %>",
   :disabled => false
 )
