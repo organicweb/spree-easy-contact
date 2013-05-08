5f94e791012ac2cb71eca21c904ce89bbce5bdc8
leosmeira <leosmeira@gmail.com>
2013-05-08 00:08:29 -0300

    mass-assign protected attributes: topic_id, name, order_number, email, message

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
 class Contact < ActiveRecord::Base
   
+  attr_accessible :topic_id, :name, :order_number, :email, :message
+  
   validates :name, :presence => true
   validates :email, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
   validates :order_number, :format => {:with => /(^$)|(^R\d{9}$)/i, :message => I18n.t("invalid_order_number")}
