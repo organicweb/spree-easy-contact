7a7154f8264a7b0fef19ae7785bddd6498a71c0e
leosmeira <leosmeira@gmail.com>
2013-05-07 23:58:18 -0300

    remove recaptcha

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,7 +12,7 @@ class ContactsController < Spree::BaseController
   def create
     @contact = Contact.new(params[:contact] || {})
     respond_to do |format|
-      if verify_recaptcha(model: @contact) && @contact.valid? &&  @contact.save
+      if @contact.valid? &&  @contact.save
         ContactMailer.message_email(@contact).deliver
         format.html { redirect_to(root_path, :notice => t("message_sended")) }
       else
