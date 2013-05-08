a8ad44282ff4bfd9f837231ecc1d17d6d7ae91f0
leosmeira <leosmeira@gmail.com>
2013-05-07 22:56:13 -0300

    Bug of generation migration

--- lib/generators/spree_easy_contact/install/install_generator.rb
+++ lib/generators/spree_easy_contact/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeEasyContact
     class InstallGenerator < Rails::Generators::Base
 
       def add_migrations
-        run 'rake railties:install:migrations FROM=spree_easy_contact'
+        run 'bundle exec rake railties:install:migrations FROM=spree-easy-contact'
       end
 
       def run_migrations
          res = ask "Would you like to run the migrations now? [Y/n]"
          if res == "" || res.downcase == "y"
-           run 'rake db:migrate'
+           run 'bundle exec rake db:migrate'
          else
            puts "Skiping rake db:migrate, don't forget to run it!"
          end
