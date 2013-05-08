745bd9c81eb7dfb63a070b61107a56f3020f77e4
leosmeira <leosmeira@gmail.com>
2013-05-07 23:16:05 -0300

    Rename migration gem

--- lib/generators/spree_easy_contact/install/install_generator.rb
+++ lib/generators/spree_easy_contact/install/install_generator.rb
@@ -3,7 +3,7 @@ module SpreeEasyContact
     class InstallGenerator < Rails::Generators::Base
 
       def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=spree-easy-contact'
+        run 'bundle exec rake railties:install:migrations FROM=spree_easy_contact'
       end
 
       def run_migrations
