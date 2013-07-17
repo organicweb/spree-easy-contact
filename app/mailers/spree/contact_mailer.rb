c9a2f4cb79eb72afde009632e2db677830e7aca4
Mathias <mathias@organicweb.fr>
2013-07-17 18:51:03 +0200

    translation for mailer views for mailer

--- app/mailers/spree/contact_mailer.rb
+++ app/mailers/spree/contact_mailer.rb
@@ -1,11 +1,12 @@
 module Spree
   class ContactMailer < ActionMailer::Base
+
     
     def notification contact
       @contact = contact
       
       mail(
-        :to => contact.topic.email,
+        :to => site_owner_email,
         :reply_to => contact.email,
         :from => mail_from,
         :subject => "#{Spree::Config[:site_name]} : #{contact.topic.title}"
@@ -25,11 +26,11 @@ module Spree
     
     private
     def mail_from
-      Spree::Config[:mails_from]
+      Spree::MailMethod.current.try(:preferred_mails_from)
     end
   
     def site_owner_email
-      Spree::Config[:mails_from]
+      Spree::MailMethod.current.try(:preferred_mails_from)
     end
   end
 end
--- app/models/spree/contact.rb
+++ app/models/spree/contact.rb
@@ -3,6 +3,12 @@ module Spree
     belongs_to :topic
     attr_accessible :email, :message, :name, :topic_id, :order_number
   
-    validates :name, :email, :topic_id, :message, :presence => true
+    validates :name, :topic_id, :message, :presence => true
+    validates :email, :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+    
+    after_save do
+      ContactMailer.notification(self).deliver # to submitter
+      ContactMailer.confirmation(self).deliver # to admin
+    end
   end
 end
